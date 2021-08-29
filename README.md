a34dcc70df9584b6fd74f0dda311fde8f717cd65
senichimaro <senichimaro@gmail.com>
2021-08-29 22:48:48 +0200

    step 3 : todo app > init : insert todos and view representation

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This exercise creates a TODOs app that is a basic CRUD operations app.
 1. [x] this first step builds the server a serves an index.html that render a data object JSON like: a python list.
 
 2. [x] this second step replace the data object with a SQLAlchemy operation that query for all the row in a given table. A database connection is setup.
+
+3. [x] Now let's modify this app to get a Todo App. The model is a simple database that have a single table which saves one field called 'description'. The view is one input in a form which targets '/todos/create' with POST method and a list item render. The controller catch the request body, create the record in the database, and redirects (by function name) to index to see this object loaded.
--- app.py
+++ app.py
@@ -1,36 +1,86 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+import sys
 import os
 load_dotenv()
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 app = Flask(__name__)
+
+# Model
+# ---------------------------------------------------------------
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Professor(db.Model):
+# mpdel
+class Todo(db.Model):
+    __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    area = db.Column(db.String())
+    description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return f'<Professor ID: {self.id} name: {self.name} >'
+        return f'<Todo ID: {self.id} name: {self.description} >'
 
 db.create_all()
 
 
+
+# Controllers
+# ----------------------------------------------------------------
+
+# INDEX
 @app.route('/')
 def index():
-    prof = Professor.query.all()
-    # prof = [
-    #     { "name":'Albert' },
-    #     { "name":'Camus' },
-    #     { "name":'George' },
-    #     { "name":'Diego' },
-    # ]
-    return render_template('index.html', data=prof)
+    return render_template('index.html', data=Todo.query.all())
 
+# POST
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    # Pattern Error Handling
+    error = False
+
+    # body is used to avoid
+    # the error of access at db objects
+    # after close the session.
+    body = {}
+    try:
+        # desc = request.form.get('description') # synchronous
+        desc = request.get_json()['description'] # asynchronous
+        todo = Todo(description=desc)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        # return redirect(url_for('index')) # synchronous
+        # return jsonify({
+        #     'description': body
+        # }) # asynchronous
+        return jsonify(body) # asynchronous
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Access
 if __name__ == '__main__':
     app.run()
